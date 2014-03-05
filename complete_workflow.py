57f38715a6cac539cdc1fdbaca22b31cd7dc34a6
ralili <mrullan.soc.jo@gmail.com>
2014-03-05 18:32:10 +0100

    update complete_workflow.py. Addition of whole led operation

--- complete_workflow.py
+++ complete_workflow.py
@@ -1,12 +1,9 @@
 import CFlow,time,threading
-a=CFlow.operate_arduino()
-b=CFlow.click()
-c=CFlow.click()
 
 #CFLOW STARTUP
 #PUMP CALIBRATION
 #PUMP STARTUP
-def bring_sample(operate_arduino_object):			#a is a CFlow.operate_arduino() object
+def bring_sample(operate_arduino_object):
   operate_arduino_object.sample_to_cytometer(1.4)##How much sample to take
   operate_arduino_object.air_to_cytometer(2)
   operate_arduino_object.cytometer_to_waste(3)
@@ -26,7 +23,8 @@ def bring_sample(operate_arduino_object):			#a is a CFlow.operate_arduino() obje
   operate_arduino_object.turn_off_valve('valve2')
   return
 
-def pumping_operation(day,hour,minute,frequency,num_samples,operate_arduino_object,click_object):
+def pumping_operation(day,hour,minute,frequency,num_samples,operate_arduino_object):
+  click_object=CFlow.click()
   click_object.set_measuring_times(day,hour,minute,frequency,num_samples)		#Set input correctly. day,hour,minute,frequency,num_samples
   while click_object.set_waiting_time()==0:
     time.sleep(click_object.waiting_time)
@@ -41,18 +39,25 @@ def pumping_operation(day,hour,minute,frequency,num_samples,operate_arduino_obje
     operate_arduino_object.cytometer_to_waste(7)
   return
 
-def led_operation(operate_arduino_object)
-  operate_arduino_object.operate_led('led1',256)
-  time.sleep(2000)
-  operate_arduino_object.operate_led('led1',0)
-  operate_arduino_object.operate_led('led2',256)
-  time.sleep(2000)
-  operate_arduino_object.operate_led('led2',0)
+def led_operation(day,hour,minute,frequency,operate_arduino_object)
+  led=CFlow.led(operate_arduino_object)
+  led.read_intensity_file()
+  led.scale_intensities()
+  led.set_change_times(day,hour,minute,frequency)
+  led.guide()
   return
 
-thread1=threading.Thread(target=pumping_operation, args = (day,hour,minute,frequency,num_samples,a,b))
-thread2=threading.Thread(target=ked_operation, args = (a))
+operate_arduino_object=CFlow.operate_arduino()
+click_object=CFlow.click()
+day=
+hour=
+minute=
+frequency_sampling=
+samples=
+frequency_light=
+  
+
+thread1=threading.Thread(target=pumping_operation, args = (day,hour,minute,frequency_sampling,num_samples,operate_arduino_object,click_object))
+thread2=threading.Thread(target=led_operation, args = (day,hour,minute,frequency_light,operate_arduino_object))
 thread1.start()
 thread2.start()
-
-pumping_operation(day,hour,minute,frequency,num_samples,a,b)
