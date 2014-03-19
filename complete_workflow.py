41546f0d29d73f86ae6f2abd61b203272956a86c
ralili <mrullan.soc.jo@gmail.com>
2014-03-19 10:44:10 +0100

    Additions done to run proportional feedback on 18.03.2014

--- complete_workflow.py
+++ complete_workflow.py
@@ -2,19 +2,19 @@ import CFlow,time,threading
 
 operate_arduino_object=CFlow.operate_arduino()
 
-day=13#starting day
-hour=18#starting hour
-minute=33#starting minute
+day=18#starting day
+hour=12#starting hour
+minute=48#starting minute
 frequency_sampling=10#frequency of cytometer measurements, in minutes
-samples=12#number of cytometer measurements in total
+samples=60#number of cytometer measurements in total
 frequency_light=3600#frequency of led changes, in seconds
-start_sample_well=19
+start_sample_well=0
 
 #CFLOW STARTUP
 #PUMP CALIBRATION
 #PUMP STARTUP
 def bring_sample(operate_arduino_object):
-  operate_arduino_object.sample_to_cytometer(1.2)##How much sample to take. 1.2
+  operate_arduino_object.sample_to_cytometer(1.3)##How much sample to take. 1.2
   operate_arduino_object.air_to_cytometer(1)#2
   operate_arduino_object.cytometer_to_waste(3)#3
   operate_arduino_object.push_to_cytometer(2.5)#2
@@ -37,6 +37,7 @@ def pumping_operation(day,hour,minute,frequency,num_samples,operate_arduino_obje
   click_object=CFlow.click(sample_counter=starting_well)
   click_object.set_measuring_times(day,hour,minute,frequency,num_samples)		#Set input correctly. day,hour,minute,frequency,num_samples
   read_fcs_object=CFlow.read_fcs('C:\\Users\\rumarc\\Desktop\\Results')
+  operate_arduino_object.operate_led(1,256)####
   while click_object.set_waiting_time()==0:
     time.sleep(click_object.waiting_time)
     print(click_object.waiting_time)
@@ -44,10 +45,19 @@ def pumping_operation(day,hour,minute,frequency,num_samples,operate_arduino_obje
     click_object.sample()
     click_object.run()
     click_object.export()
+    time.sleep(1)
     click_object.moveFiles('C:\\Users\\rumarc\\Desktop','C:\\Users\\rumarc\\Desktop\\Results')###THIS CHANGES FROM COMPUTER TO COMPUTER. THE OUTPUT FOLDER MUST BE CREATED BEFOREHAND
+    time.sleep(1)
     ##PERFORM FEEDBACK
     YFP_mean=read_fcs_object.get_last_data(click_object)
-    LED_signal=0.05*(YFP_mean-0.5)##SET REFERENCE, SET P PARAMETER
+    print(YFP_mean)
+    LED_signal=0.8*(0.66-YFP_mean)##SET REFERENCE, SET P PARAMETER
+    if LED_signal<0:
+        LED_signal=0
+    elif LED_signal>1:
+        LED_signal=1
+    print(LED_signal)
+    LED_signal=LED_signal*256
     operate_arduino_object.operate_led(2,LED_signal)
 	##
     click_object.backflush()
@@ -68,5 +78,5 @@ def led_operation(day,hour,minute,frequency,operate_arduino_object):#frequency i
 thread1=threading.Thread(target=pumping_operation, args = (day,hour,minute,frequency_sampling,samples,operate_arduino_object,start_sample_well))
 thread1.start()
 
-thread2=threading.Thread(target=led_operation, args = (day,hour,minute,frequency_light,operate_arduino_object))
-thread2.start()
+#thread2=threading.Thread(target=led_operation, args = (day,hour,minute,frequency_light,operate_arduino_object))
+#thread2.start()
