5c26c02685283b0deebf8905d1b536ead1e945a7
ralili <mrullan.soc.jo@gmail.com>
2014-04-03 10:18:11 +0200

    addition of code documentation.

--- extract_from_log.py
+++ extract_from_log.py
@@ -1,26 +1,22 @@
 import os,re,csv
-os.chdir('C:/Users/rumarc/Desktop/LBperturbation25032014')
-csvfile=open('res.csv','w')
+os.chdir('C:/Users/rumarc/Desktop')		#Folder where log file is located
+csvfile=open('results.csv','w')			#Name of output file
 csvwriter = csv.writer(csvfile, dialect='excel')
-res1=open('CFlow_execution25032014(1).log','r')
+res1=open('CFlow_execution.log','r')	#Name of log file
 res1lines=res1.readlines()
 
+##Stuff that you want to get from the log file
 YFP=['YFP']+[m.group(1) for line in res1lines for m in [re.search('YFP mean is: ([0-9.-]+)',line)] if m]
+reference=['Reference']+[m.group(1) for line in res1lines for m in [re.search('reference is: ([0-9.-]+)',line)] if m]
 Integral=['Integral']+[m.group(1) for line in res1lines for m in [re.search('Integral parameter value is: ([0-9.-]+)',line)] if m]
 Proportional=['Proportional']+[m.group(1) for line in res1lines for m in [re.search('Proportional parameter value is: ([0-9.-]+)',line)] if m]
 LED=['LED']+[m.group(1) for line in res1lines for m in [re.search('LED signal is: ([0-9.-]+)',line)] if m]
+Time=['Time']+[(i)*10 for i in range(len(YFP)-1)]
 
-res2=open('CFlow_execution25032014(2).log','r')
-res2lines=res2.readlines()
-
-YFP=YFP+[m.group(1) for line in res2lines for m in [re.search('YFP mean is: ([0-9.-]+)',line)] if m]
-Integral=Integral+[m.group(1) for line in res2lines for m in [re.search('Integral parameter value is: ([0-9.-]+)',line)] if m]
-Proportional=Proportional+[m.group(1) for line in res2lines for m in [re.search('Proportional parameter value is: ([0-9.-]+)',line)] if m]
-LED=LED+[m.group(1) for line in res2lines for m in [re.search('LED signal is: ([0-9.-]+)',line)] if m]
-Time=['Time']+[(i+2)*10 for i in range(len(LED)-1)]
-
+##Write information extracted from the log file into a csv file. Csv file is created in the folder where the log file is located.
 csvwriter.writerow(Time)
 csvwriter.writerow(YFP)
+csvwriter.writerow(reference)
 csvwriter.writerow(Integral)
 csvwriter.writerow(Proportional)
 csvwriter.writerow(LED)
