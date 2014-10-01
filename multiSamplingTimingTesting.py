23186e3705ac76af7f2b67f1117643b600cc780b
ralili <mrullan.soc.jo@gmail.com>
2014-10-01 13:04:07 +0200

    addition of breakpoint between filling sample tube and taking measurement, necessary to empty the push liquid from the tube

--- multiSamplingTimingTesting.py
+++ multiSamplingTimingTesting.py
@@ -1,6 +1,6 @@
 ##FILE TO GET THE TIMING FOR THE MULTISAMPLING CORRECT
 import CFlow,yaml,os
-os.chdir("C:\\Users\\rumarc\\Desktop\\multisampling")#CHANGE
+os.chdir("C:\\Users\\rumarc\\Desktop\\Results")#CHANGE
 arduinoObject=CFlow.multiOperate_arduino()
 
 with open('setTiming.yaml', 'r') as f:
@@ -8,8 +8,8 @@ with open('setTiming.yaml', 'r') as f:
 
 
 airTimes1=[]
-pbsTimes1=[]
 sampleTimes=[]
+pbsTimes1=[]
 pbsTimes2=[]
 airTimes2=[]
 backSampleTimes=[]
@@ -17,21 +17,26 @@ backSampleTimes=[]
 for i in range(len(doc)):
   for key in doc[i].keys():
     airTimes1.append(doc[i][key]['air1'])
-    pbsTimes1.append(doc[i][key]['pbs1'])
     sampleTimes.append(doc[i][key]['sample'])
+    pbsTimes1.append(doc[i][key]['pbs1'])
     pbsTimes2.append(doc[i][key]['pbs2'])
     airTimes2.append(doc[i][key]['air2'])
     backSampleTimes.append(doc[i][key]['back sample'])
 
-def bring_sample(operate_arduino_object,sample,airTimes1,pbsTimes1,sampleTimes,pbsTimes2,airTimes2,backSampleTimes):
+def bring_sample(operate_arduino_object,sample,airTimes1,sampleTimes,pbsTimes2,airTimes2,backSampleTimes):
   sample=sample-1
   operate_arduino_object.air_to_cytometer(airTimes1[sample])
-  operate_arduino_object.push_to_cytometer(pbsTimes1[sample])
+  raw_input()
   operate_arduino_object.sample_to_cytometer(sample+1,sampleTimes[sample])
+  raw_input()
+  operate_arduino_object.push_to_cytometer(pbsTimes1[sample])
+  raw_input()
   operate_arduino_object.push_to_cytometer(pbsTimes2[sample])
+  raw_input()
   operate_arduino_object.air_to_cytometer(airTimes2[sample])
+  raw_input()
   operate_arduino_object.cytometer_to_sample(sample+1,backSampleTimes[sample])
   operate_arduino_object.turn_off_valves()
   return
 
-bring_sample(arduinoObject,2,airTimes1,pbsTimes1,sampleTimes,pbsTimes2,airTimes2,backSampleTimes)
+bring_sample(arduinoObject,1,airTimes1,sampleTimes,pbsTimes2,airTimes2,backSampleTimes)
