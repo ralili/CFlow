2ecdd6be57c1b356c6cdd8204aa0e7eb33a3e915
ralili <mrullan.soc.jo@gmail.com>
2014-10-07 11:35:20 +0200

    change of directories and sampleHandling call functions

--- complete_workflow_multi.py
+++ complete_workflow_multi.py
@@ -1,23 +1,22 @@
 import CFlow,time,threading,logging
 '''for multisampling'''
-logging.basicConfig(filename='C:\\Users\\rumarc\\Desktop\\Results\\CFlow_execution.log', level=logging.INFO,filemode='w',format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H:%M')
-arduinoObject=CFlow.multiOperate_arduino()
+logging.basicConfig(filename='C:\\Users\\localadmin\\Desktop\\Results\\CFlow_execution.log', level=logging.INFO,filemode='w',format='%(asctime)s %(message)s',datefmt='%Y-%m-%d %H:%M')
+operate_arduino_object=CFlow.multiOperate_arduino()
 
-day=2#starting day
-hour=18#starting hour
+day=7#starting day
+hour=11#starting hour
 minute=20#starting minute
-frequency_sampling=[5,4,7,2]#frequency of cytometer measurements, in minutes
-num_samples=[3,4,3,7]#number of cytometer measurements in total
+frequency_sampling=[5,5,0,0]#frequency of cytometer measurements, in minutes
+num_samples=[3,3,0,0]#number of cytometer measurements in total
 starting_well=0
 
-def pumping_operation(day,hour,minute,frequency_sampling,num_samples,operate_arduino_object,samplingObject,starting_well=0):
+def pumping_operation(day,hour,minute,frequency_sampling,num_samples,operate_arduino_object,starting_well=0):
   samplingObject=CFlow.sampleHandling()
-  samplingObject.setTimes(folder)
 
   click_object=CFlow.click(sample_counter=starting_well)
   click_object.set_measuring_times(day,hour,minute,frequency_sampling,num_samples)		#Set input correctly. day,hour,minute,frequency,num_samples
 
-  read_fcs_object=CFlow.read_fcs('C:\\Users\\rumarc\\Desktop\\Results','ecoli')
+  read_fcs_object=CFlow.read_fcs('C:\\Users\\localadmin\\Desktop\\Results','ecoli')
 
   while click_object.set_waiting_time()==0:
     time.sleep(click_object.waiting_time)
@@ -27,7 +26,7 @@ def pumping_operation(day,hour,minute,frequency_sampling,num_samples,operate_ard
     click_object.run()
     click_object.export()
     time.sleep(1)
-    click_object.moveFiles('C:\\Users\\rumarc\\Desktop','C:\\Users\\rumarc\\Desktop\\Results')###THIS CHANGES FROM COMPUTER TO COMPUTER. THE OUTPUT FOLDER MUST BE CREATED BEFOREHAND
+    click_object.moveFiles('C:\\Users\\localadmin\\Desktop','C:\\Users\\localadmin\\Desktop\\Results')###THIS CHANGES FROM COMPUTER TO COMPUTER. THE OUTPUT FOLDER MUST BE CREATED BEFOREHAND
     time.sleep(1)
     ##PERFORM FEEDBACK
     ##
